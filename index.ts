881bd14d66b87a084d00f519510186f9441536c7
cplepage <lepage.charlesphilippe@gmail.com>
2025-10-15 10:55:31 -0400

    exploration

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { createClass } from "./style";
+
+document.title = "Fullstacked Style";
+
+const redClass = createClass("red", {
+    backgroundColor: "red",
+    color: "white",
+    fontFamily: "sans-serif",
+    padding: 5,
+    width: 200,
+});
+
+const div = document.createElement("div");
+div.classList.add(redClass);
+div.innerHTML = `<span>Hello</span>`;
+document.body.append(div);
---
+++ style.ts
@@ -0,0 +1,56 @@
+import CSS from "csstype";
+import { partial } from "zod/v4/core/util";
+
+const style = document.createElement("style");
+document.head.append(style);
+
+const propertiesDefaultingToPx: { [property in keyof CSS.Properties]: true } = {
+    padding: true,
+    paddingTop: true,
+    paddingRight: true,
+    paddingBottom: true,
+    paddingLeft: true,
+
+    margin: true,
+    marginTop: true,
+    marginRight: true,
+    marginBottom: true,
+    marginLeft: true,
+
+    width: true,
+    height: true,
+
+    top: true,
+    right: true,
+    bottom: true,
+    left: true,
+
+    gap: true
+} as const;
+
+const propertiesDefaultingToPxArr = Object.keys(propertiesDefaultingToPx);
+
+type CSSProperties =
+    | CSS.Properties
+    | {
+          [property in keyof typeof propertiesDefaultingToPx]: number | CSS.Properties[property];
+      };
+
+export function createClass(name: string, cssProperties: CSSProperties) {
+    const tempElement = document.createElement("div");
+
+    Object.entries(cssProperties).forEach(([property, value]) => {
+        if(propertiesDefaultingToPxArr.includes(property) && value && typeof value === "number") {
+            value = value + "px"
+        }
+
+        tempElement.style[property] = value;
+    });
+
+    const cssString = tempElement.style.cssText;
+    tempElement.remove();
+
+    style.innerText += `.${name}{${cssString}}`;
+
+    return name;
+}
